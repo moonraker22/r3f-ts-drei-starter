94d861a877ca06844d97059789447c5ce8ab7c63
MooNRakeR <zxkst2@protonbmail.com>
2023-02-25 10:48:45 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Three Fiber Starter
+
+A starter template for quick spinup of a react three fiber with vite and typescript
+
+## Authors
+
+- [@moonraker22](https://www.github.com/moonraker22)
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Group } from 'three'
+import './App.css'
+import './styles.css'
+
+const Scene = () => {
+  const scene = useRef<Group>(null!)
+  useFrame(() => {
+    if (!scene.current) return
+    scene.current.rotation.y += 0.04
+    scene.current.rotation.x += 0.04
+    scene.current.rotation.z += 0.04
+  })
+  return (
+    <group ref={scene}>
+      <Box>
+        <meshLambertMaterial attach="material" color="white" />
+      </Box>
+    </group>
+  )
+}
+
+function App() {
+  return (
+    <Canvas>
+      <directionalLight intensity={0.5} />
+      <Scene />
+    </Canvas>
+  )
+}
+
+export default App
