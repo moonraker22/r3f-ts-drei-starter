628dcff4934fd8942d6af762a392ff8086c634c5
MooNRakeR <zxkst2@protonbmail.com>
2023-02-25 11:21:19 -0600

    glsl plugin options

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,24 @@ import glsl from 'vite-plugin-glsl'
 export default defineConfig({
   plugins: [glsl()],
 })
+
+// Glsl plugin options
+// https://github.com/UstymUkhman/vite-plugin-glsl
+
+// glsl({
+//   include: [
+//     // Glob pattern, or array of glob patterns to import
+//     '**/*.glsl',
+//     '**/*.wgsl',
+//     '**/*.vert',
+//     '**/*.frag',
+//     '**/*.vs',
+//     '**/*.fs',
+//   ],
+//   exclude: undefined, // Glob pattern, or array of glob patterns to ignore
+//   warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
+//   defaultExtension: 'glsl', // Shader suffix when no extension is specified
+//   compress: false, // Compress output shader code
+//   watch: true, // Recompile shader on change
+//   root: '/', // Directory for root imports
+// })
